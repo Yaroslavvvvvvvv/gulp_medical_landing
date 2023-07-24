62b2ad728b05a36335ab2764b2cde44c8af8fefa
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-07-25 00:04:58 +0300

    add scroll, fix mobile arror

---
+++ app/js/script.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+
+    const hash = window.location.hash
+    const header = $('header')
+    const headerHeight = header.outerHeight()
+
+    scrollToElement(hash)
+
+    function scrollToElement(selectorToScrollElement, callback = null) {
+        const body = $("html, body")
+
+        if (!selectorToScrollElement) return
+
+        const scrollTo = ($(selectorToScrollElement).offset().top) - headerHeight
+
+        $('header nav .nav-item.active').removeClass('active')
+
+
+        body.stop().animate({scrollTop: scrollTo}, 500, 'swing', () => {
+            $(`a.nav-link[href="${selectorToScrollElement}"]`).parent().addClass('active')
+            if (callback) callback()
+        })
+    }
+
+
+    $('header nav .nav-item .nav-link').click(function () {
+        scrollToElement($(this).attr('href'))
+    })
+
+    $('a.navbar-brand').click(function () {
+        scrollToElement($(this).attr('href'), () => {
+            window.location.hash = ''
+        })
+    })
+
+    $(window).scroll(function () {
+        const scrollTo = $(window).scrollTop()
+
+        if (scrollTo >= headerHeight) header.addClass('shadow-sm')
+        else header.removeClass('shadow-sm')
+    });
+
+
+})
+
+
+
