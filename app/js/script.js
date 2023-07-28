87518ccc836096cecce1a59e84fafba540a2a7a1
Yaroslav Kovalchuk <dantekip972@gmail.com>
2023-07-29 01:08:36 +0300

    add slider in fifth section

--- app/js/script.js
+++ app/js/script.js
@@ -44,4 +44,27 @@ $(document).ready(function () {
 })
 
 
+$(document).ready(function () {
+    const $slides = $(".slide");
+    let currentIndex = 0;
+
+    function showSlide(index) {
+        $slides.hide().eq(index).show();
+    }
+
+    function prevSlide() {
+        currentIndex = (currentIndex - 1 + $slides.length) % $slides.length;
+        showSlide(currentIndex);
+    }
+
+    function nextSlide() {
+        currentIndex = (currentIndex + 1) % $slides.length;
+        showSlide(currentIndex);
+    }
+
+    $(".prev").on("click", prevSlide);
+    $(".next").on("click", nextSlide);
+});
+
+
 
